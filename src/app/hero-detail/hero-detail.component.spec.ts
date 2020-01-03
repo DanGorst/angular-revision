6068f81c51b89636daa86882504565bf0df0ecf9
Daniel Gorst <dgorst@scottlogic.com>
2020-01-03 11:28:14 +0000

    Adds tests for hero detail component
    Also updates how service dependency is pulled in - to match Angular best
    practices.

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -7,31 +7,26 @@ import { ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
 import { of } from 'rxjs';
 import { HeroService } from '../hero.service';
+import { By } from '@angular/platform-browser';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
 
-  let mockHeroService;
-  let updateHeroesSpy;
-  let getHeroSpy;
-
-  let mockLocation;
-
-  let activatedRouteStub: ActivatedRouteStub;
+  let heroService;
 
   beforeEach(async(() => {
-    mockHeroService = jasmine.createSpyObj('HeroService', ['getHero', 'updateHero']);
-    updateHeroesSpy = mockHeroService.updateHero.and.returnValue(of({}));
-    getHeroSpy = mockHeroService.getHero.and.returnValue(of({
+    const mockHeroService = jasmine.createSpyObj('HeroService', ['getHero', 'updateHero']);
+    mockHeroService.updateHero.and.returnValue(of({}));
+    mockHeroService.getHero.and.returnValue(of({
       id: 1, name: 'testHero'
     }));
 
-    mockLocation = {
+    const mockLocation = {
       back: jasmine.createSpy('back')
     };
 
-    activatedRouteStub = new ActivatedRouteStub({ id: '1' });
+    const activatedRouteStub = new ActivatedRouteStub({ id: '1' });
 
     TestBed.configureTestingModule({
       imports: [ FormsModule ],
@@ -53,5 +48,30 @@ describe('HeroDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update title when user enters new hero name', () => {
+    const nameInput: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    const nameDisplay: HTMLElement = fixture.nativeElement.querySelector('h2');
+
+    nameInput.value = 'New Hero';
+    nameInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(nameDisplay.textContent).toBe('NEW HERO Details');
+  });
+
+  it('should update hero name via service when user selects to save it', () => {
+    heroService = TestBed.get(HeroService);
+
+    const nameInput: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    const saveButton = fixture.debugElement.query(By.css('#save-button'));
+
+    nameInput.value = 'New Hero';
+    nameInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    saveButton.triggerEventHandler('click', null);
+    expect(heroService.updateHero).toHaveBeenCalledWith({ id: 1, name: 'New Hero' });
   });
 });
