aa680b9474f2b0514c0146bfa24470a4dad8fcd3
Dan <daniel.gorst81@googlemail.com>
2020-01-10 14:58:35 +0000

    Adds built-in and custom validators to the form

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { forbiddenNameValidator } from './forbidden-name.directive';
 
 @Component({
   selector: 'app-profile-editor',
@@ -9,8 +10,8 @@ import { FormBuilder, Validators, FormArray } from '@angular/forms';
 export class ProfileEditorComponent implements OnInit {
 
   profileForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: [''],
+    firstName: ['', [Validators.required, forbiddenNameValidator(/bob/i)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
     address: this.fb.group({
       street: [''],
       city: [''],
@@ -47,5 +48,13 @@ export class ProfileEditorComponent implements OnInit {
 
   addAlias() {
     this.aliases.push(this.fb.control(''));
+  }
+
+  get lastName() {
+    return this.profileForm.get('lastName') as FormControl;
+  }
+
+  get firstName() {
+    return this.profileForm.get('firstName') as FormControl;
   }
 }
